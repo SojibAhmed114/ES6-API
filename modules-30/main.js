6418b21b7591d5f8e622a65df930f6bd90072c32
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-02-24 23:04:14 +0600

    modules 30 ar 30.1 done

--- modules-30/main.js
+++ modules-30/main.js
@@ -1,3 +1,20 @@
 const numbers =[25, 84, 96, 54, 78, 45];
 numbers[1] = 100;
+numbers[3] = 500;
+numbers.push(200, 200, 200);
 console.log(numbers); 
+
+const student = {
+    name: "sojib",
+    class: 12
+}
+
+student.name = "safi ahmed";
+console.log(student); 
+
+let sum = 0;
+for (let index = 0; index < 10; index++) {
+    const num = index;
+    sum += num;
+}
+console.log(sum); 
