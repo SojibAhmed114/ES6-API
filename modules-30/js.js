be3585c8554b68a8928f3a7f41cbe7c22be84c5a
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-02-26 12:18:28 +0600

    default parameter

---
+++ modules-30/js.js
@@ -0,0 +1,6 @@
+let num1 = 50;
+let num2 = 40;
+let num3 = num1 + num2;
+num3 += 10;
+console.log(num3);
+  
--- modules-30/main.js
+++ modules-30/main.js
@@ -17,4 +17,28 @@ for (let index = 0; index < 10; index++) {
     const num = index;
     sum += num;
 }
-console.log(sum); 
+
+function checks (num1, num2) {
+    const results = num1 + num2;
+    console.log(num1, num2, results);
+    return results;
+}
+const values = checks(40, 58);
+
+// dafault parameter numbers values 
+function defaults(names1, names=5) {
+    const total = names1 + names;
+    console.log(names1, names, total); 
+    return total;
+}
+const value2 = defaults(5, 10);
+
+// dafault parameter string values 
+function fullName(firstName, lastName = "") {
+    const fullNames = firstName + " " + lastName;
+    console.log(fullNames); 
+    return fullNames;
+}
+const useNames = fullName("sojib");
+
+// dafault parameter array values 
