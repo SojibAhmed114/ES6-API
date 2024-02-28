aae262e0c400bc269e9fe245905b60c1eb142d6a
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-02-28 12:40:24 +0600

    task full compilied

--- modules-30/task.js
+++ modules-30/task.js
@@ -2,7 +2,20 @@ let numberOne, numberTwo, numbeThree;
 [numberOne, numberTwo, numbeThree] = [50, 100, 600];
 console.log(numbeThree);
 
-// task one 
+// task 1
 const multi = (num1, num2, num3) => num1 * num2 * num3;
 const setData = multi(2, 3, 4);
 console.log("setData: ", setData);
+
+//......
+const multiLine = `I am a web developer
+I love code
+I love to eat biriyani`;
+console.log(multiLine);
+
+//...........
+const sum = (numOne, numTwo = 500) => numOne + numTwo;
+const setData2 = sum(40);
+console.log("setData2: ", setData2);
+
+
