ec0f8c75b2a9340f0e97080d17c6ca473d99e4f6
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-02-28 12:27:13 +0600

    task one done

---
+++ modules-30/task.js
@@ -0,0 +1,8 @@
+let numberOne, numberTwo, numbeThree;
+[numberOne, numberTwo, numbeThree] = [50, 100, 600];
+console.log(numbeThree);
+
+// task one 
+const multi = (num1, num2, num3) => num1 * num2 * num3;
+const setData = multi(2, 3, 4);
+console.log("setData: ", setData);
